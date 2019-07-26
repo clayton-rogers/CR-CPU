5206f8583b1dda39631aeaf98ac248c7e87be7f2
Clayton Rogers <claytonrogers53@gmail.com>
2019-07-26 00:57:55 -0400

    Moved opcode constants to header file

--- fpga/ALU.v
+++ fpga/ALU.v
@@ -8,22 +8,7 @@ module ALU (i_opcode, i_extra, i_data1, i_data2, i_const, o_data);
   input [7:0] i_const;
   output reg [15:0] o_data;
 
-  localparam ADD    = 4'h0;
-  localparam SUB    = 4'h1;
-  localparam AND    = 4'h2;
-  localparam OR     = 4'h3;
-  localparam SHIFT  = 4'h4;
-  localparam LOAD   = 4'h5;
-  localparam STORE  = 4'h6;
-  localparam MOVE   = 4'h7;
-  localparam JUMP   = 4'h8;
-  localparam LOADC  = 4'h9;
-  localparam UNDEF1 = 4'hA;
-  localparam UNDEF2 = 4'hB;
-  localparam UNDEF3 = 4'hC;
-  localparam UNDEF4 = 4'hD;
-  localparam UNDEF5 = 4'hE;
-  localparam UNDEF6 = 4'hF;
+  `include "opcodes.vh"
 
   // For add, sub, and, or
   wire operant2_switch = i_extra[0];
---
+++ fpga/opcodes.vh
@@ -0,0 +1,17 @@
+
+localparam ADD    = 4'h0;
+localparam SUB    = 4'h1;
+localparam AND    = 4'h2;
+localparam OR     = 4'h3;
+localparam SHIFT  = 4'h4;
+localparam LOAD   = 4'h5;
+localparam STORE  = 4'h6;
+localparam MOVE   = 4'h7;
+localparam JUMP   = 4'h8;
+localparam LOADC  = 4'h9;
+localparam UNDEF1 = 4'hA;
+localparam UNDEF2 = 4'hB;
+localparam UNDEF3 = 4'hC;
+localparam UNDEF4 = 4'hD;
+localparam UNDEF5 = 4'hE;
+localparam UNDEF6 = 4'hF;
