74fc1a926794347052a5f81e04bc93bad4b2f0b6
Clayton Rogers <claytonrogers53@gmail.com>
2019-08-14 13:05:59 -0400

    Added more test cases

--- cr-cc/cr-cc-test/TEST_assembler.cpp
+++ cr-cc/cr-cc-test/TEST_assembler.cpp
@@ -23,12 +23,35 @@ TEST_CASE("Test assembler instructions", "[asm]") {
 		{"", ""},
 		{" ", ""},
 		{"# test comment", ""},
+		{"xor ra, ra, rb # test comment", "4000 "},
+		{"add ra, ra, rb", "0000 "},
+		{"sub ra, ra, rb", "1000 "},
+		{"and ra, ra, rb", "2000 "},
+		{"or ra, ra, rb",  "3000 "},
+		{"xor ra, ra, rb", "4000 "},
+		{"shftl rb, rb", "5500 "},
+		{"shftl rb, 0x16", "5716 "},
+		{"shftr rc, 0xA1", "5AA1 "},
+		{"load rd, 10", "6F0A "},
+		{"load ra, rb, 0x10", "6110 "},
+		{"store rd, 10", "7F0A "},
+		{"store ra, rb, 0x10", "7110 "},
+		{"jmp ra, 3", "9003 "},
+		{"jmp 3", "9303 "},
+		{"jifz 0x10", "9710 "},
+		{"jinz 0x20", "9B20 "},
+		{"jgz  rc, 0xff", "9EFF "},
+		{"out ra", "B000 "},
+		{"out rd", "BC00 "},
+		{"halt", "E000 "},
+		{"nop", "F000 "},
 	};
 
 	for (const auto& test_point : test_points) {
-		std::string output = machine_inst_to_unformatted(assemble(test_point.input));
-		
 		INFO(test_point.input);
+
+		std::string output = machine_inst_to_unformatted(assemble(test_point.input));
+
 		CHECK(output == test_point.expected_out);
 	}
 }
@@ -43,6 +66,16 @@ TEST_CASE("Test assembler should throw", "[asm]") {
 		"add ra, ra, rb, 23", // too many arguments (constant)
 		"add ra, 32, rb", // invalid constant location
 		"add ra, rb, rb", // input 1 cannot be rb (only ra, rc)
+		"add ra, ra, ra", // input 2 cannot be ra (only rb, const)
+		"jinz rd, 0x10", // offset register cannot be rd (only ra, rb, rc)
+		"jinz 0x10, 0x10", // only one constant allowed
+		"out 0x10", // missing source register
+		"out", // missing source register
+		"out ra, 0x10", // too many args
+		"halt rb", // does not allow registers
+		"halt 0x10", // does not allow constants
+		"nop rb", // does not allow registers
+		"nop 0x10", // does not allow constants
 	};
 
 	for (const auto& test_point : test_points) {
@@ -60,13 +93,14 @@ TEST_CASE("Test assembler programs", "[asm]") {
 	};
 
 	for (const auto& test_point : test_points) {
+		INFO(test_point.expected_out);
+
 		std::string program = read_file(std::string("./cr-cc-test/test_data/") + test_point.input);
 
 		REQUIRE(program.length() != 0);
 
 		std::string output = machine_inst_to_unformatted(assemble(program));
 
-		INFO(test_point.expected_out);
 		CHECK(output == test_point.expected_out);
 	}
 }
