39fb1309616ef51c90e6246e2b0c79441987c6c2
Clayton Rogers <claytonrogers53@gmail.com>
2019-08-28 20:28:48 -0400

    CR-CC: Update assembler for new instruction set
    CR-CC: Rename of types
    
    CR-CC: Updated test programs

--- cr-cc/cr-cc-lib/assembler.h
+++ cr-cc/cr-cc-lib/assembler.h
@@ -3,4 +3,7 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> assemble(std::string assembly);
+std::vector<std::string> assemble(const std::string& assembly);
+
+// Returns true when the internal test passes
+bool assembler_internal_test();
--- cr-cc/cr-cc-test/TEST_assembler.cpp
+++ cr-cc/cr-cc-test/TEST_assembler.cpp
@@ -6,6 +6,11 @@
 
 #include <string>
 
+TEST_CASE("Internal assembler test", "[asm]") {
+	// Don't bother checking anything else if the internal test fails
+	REQUIRE(assembler_internal_test() == true);
+}
+
 struct Test_Point {
 	std::string input;
 	std::string expected_out;
@@ -13,42 +18,61 @@ struct Test_Point {
 
 TEST_CASE("Test assembler instructions", "[asm]") {
 	std::vector<Test_Point> test_points = {
-		{"ADD RA, RC, 10", "030A "},
-		{"loadc ra, 0x30", "A030 "},
-		{"loadc rb, 0xFF", "A4FF "},
-		{"loadc rc 32",    "A820 "},
-		{"loadc rc 32 \n .top: \n loadc rc 31 \n jmp .top", "A820 A81F 93FF "}, // jmp -1
-		{"loadch rc 0xEE", "AAEE "},
-		{"jmp .end \n .end: \n nop", "9301 F000 "}, // jmp 1
-		{"jmp 1", "9301 "},
-		{"jmp 0", "9300 "},
 		{"", ""},
 		{" ", ""},
-		{"# test comment", ""},
-		{"xor ra, ra, rb # test comment", "4000 "},
-		{"add ra, ra, rb", "0000 "},
-		{"sub ra, ra, rb", "1000 "},
-		{"and ra, ra, rb", "2000 "},
-		{"or ra, ra, rb",  "3000 "},
-		{"xor ra, ra, rb", "4000 "},
+		{"	", ""},
+		{" # test comment", ""},
+		{"xor ra, rb # test comment", "4100 "},
+		{"ADD RA, 10", "030A "},
+		{"add ra, ra", "0000 "},
+		{"sub ra, rb", "1100 "},
+		{"and ra, bp", "2200 "},
+		{"or ra, ra",  "3000 "},
+		{"xor ra, ra", "4000 "},
 		{"shftl rb, rb", "5500 "},
 		{"shftl rb, 0x16", "5716 "},
-		{"shftr rc, 0xA1", "5AA1 "},
-		{"load rd, 10", "6F0A "},
-		{"load ra, rb, 0x10", "6110 "},
-		{"store rd, 10", "7F0A "},
-		{"store ra, rb, 0x10", "7110 "},
-		{"jmp ra, 3", "9003 "},
-		{"jmp 3", "9303 "},
-		{"jifz 0x10", "9710 "},
-		{"jinz 0x20", "9B20 "},
-		{"jgz  rc, 0xff", "9EFF "},
-		{"out ra", "B000 "},
-		{"out rd", "BC00 "},
+		{"shftr bp, 0xA1", "5AA1 "},
+		{"load sp, 10", "6E0A "},
+		{"load ra, 0x10", "6210 "},
+		{"load.bp rb, 10", "640A "},
+		{"load.sp ra, -5", "61FB "},
+		{"store sp, 10", "7E0A "},
+		{"store ra, 0x10", "7210 "},
+		{"store.bp rb, 10", "740A "},
+		{"store.sp ra, -5", "71FB "},
+		{"store sp, 10", "7E0A "},
+		{"mov ra, rb", "8100 "},
+		{"mov ra, sp", "8300 "},
+		{"mov bp, sp", "8B00 "},
+		{"jmp .end \n .end: \n nop", "9201 F000 "}, // jmp abs 1
+		{"nop \n jmp.r .end \n nop \n .end: \n nop", "F000 9002 F000 F000 "}, // jmp rel +2
+		{".beg: \n nop \n jmp.r .beg", "F000 90FF "}, // jmp rel -1
+		{"jmp 1", "9201 "},
+		{"jmp 0", "9200 "},
+		{"jmp.ifz 0x10", "9610 "},
+		{".beg: \n jmp.inz .beg", "9A00 "},
+		{"jmp.inz .beg \n .beg: \n nop", "9A01 F000 "},
+		{"nop \n jmp.igz.r .beg \n .beg: \n nop", "F000 9C01 F000 "},
+		{"nop \n jmp.igz .beg \n .beg: \n nop", "F000 9E02 F000 "},
+		{"loadi ra, 0x30", "A030 "},
+		{"loadi rb, 0xFF", "A4FF "},
+		{"loadi bp 32",    "A820 "},
+		{"loadi.h sp 0xEE", "AEEE "},
+		{"in rb", "B400 "},
+		{"out ra", "B100 "},
+		{"push bp", "BA00 "},
+		{"pop sp", "BF00 "},
+		{"nop \n call .fn \n nop \n .fn: \n nop", "F000 C003 F000 F000 "}, // call abs
+		{".fn: \n nop \n call.r .fn \n nop", "F000 C1FF F000 "}, // call rel -1
+		{"nop \n call.r .fn \n nop \n .fn: \n nop", "F000 C102 F000 F000 "}, // call rel + 2
+		{"ret   ", "C200 "},
+		{"ret", "C200 "},
+		{"loada 0x45", "D045 "},
+		{"loada 10", "D00A "},
 		{"halt", "E000 "},
 		{"nop", "F000 "},
-		{"jmp -1", "93FF "},
-		{".var int[3] abc \n loadc ra, 10 \n store ra, .abc[2]","A00A 7302 "},
+		{"jmp 0xFF", "92FF "},
+		{".static 1 buf \n .static 3 abc \n loadi ra, 10 \n store ra, .abc[2]","A00A 7203 "},
 	};
 
 	for (const auto& test_point : test_points) {
@@ -63,16 +87,20 @@ TEST_CASE("Test assembler instructions", "[asm]") {
 TEST_CASE("Test assembler should throw", "[asm]") {
 	std::vector<std::string> test_points = {
 		"jmp", // missing argument
-		"add ra, ra", // missing argument
-		"add ra, 32", // missing argument (constant)
+		"add ra", // missing argument
+		"add 32", // missing argument (constant)
+		"add 32, ra", // invalid first argument
+		"add ra, ra, rb", // too many arguments
 		"add ra, ra, rb, rc", // too many arguments
-		"add ra, ra, rb, 23", // too many arguments (constant)
-		"add ra, 32, rb", // invalid constant location
-		"add ra, rb, rb", // input 1 cannot be rb (only ra, rc)
-		"add ra, ra, ra", // input 2 cannot be ra (only rb, const)
-		"jinz rd, 0x10", // offset register cannot be rd (only ra, rb, rc)
-		"jinz 0x10, 0x10", // only one constant allowed
-		"out 0x10", // missing source register
+		"add ra, ra, 23", // too many arguments (constant)
+		"add 32, rb", // invalid constant location
+		"add ra, sp", // input 2 cannot be sp
+		"move ra, rb", // invalid opcode
+		"jmp.inz.bp 0x10", // bp flag is not valid for jmp
+		"jmp.inz 0x10, 0x10", // only one constant allowed
+		"call.rp 0x01", // rp is not valid for call
+		"call .fn", // call a label that doesn't exists
+		"out 0x10", // in/out/push/pop cannot have immediate
 		"out", // missing source register
 		"out ra, 0x10", // too many args
 		"halt rb", // does not allow registers
@@ -90,8 +118,11 @@ TEST_CASE("Test assembler should throw", "[asm]") {
 }
 
 static const std::vector<Test_Point> test_programs = {
+	// Should flash alternating 0x55 and 0xAA
 	{"flasher_program.txt", "A055 A4FF B000 4000 9302 "},
+	// Should output the fibonacci sequence
 	{"fib_program.txt", "A001 A401 0800 8100 8600 B000 9302 "},
+	// Tests using labels and relative jumps. Should just count up
 	{"label.txt", "A00A 0101 B000 93FE "},
 	// Note this program should produce the output 003C when run
 	{"sum.txt", "A00A 7300 A00B 7301 A017 7302 A008 7303 A003 7304 A804 6200 0101 7200 1B01 8200 9FFB A804 A400 8100 6600 0000 1B01 8400 8200 9FFA B400 E000 " },
@@ -99,6 +130,7 @@ static const std::vector<Test_Point> test_programs = {
 	{"var_test.txt", "A00A 7300 A00B A401 0000 7301 6300 6701 0000 7300 B000 E000 "},
 	// Should produce 0x002B output
 	{"array_test.txt", "A00A 7300 A00B 7301 A016 7302 A000 6702 0400 A801 6200 0400 1B01 8200 9FFC B400 E000 "},
+	// Alternately flashes all the LEDs on and off for 400 "counts" each (~3 * 400 clocks)
 	{"flash.txt", "A0FF B000 A090 A201 1101 9FFF A000 B000 A090 A201 1101 9FFF 93F4 "},
 };
 
