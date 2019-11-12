3c9a39506d3b11729f71f4b8be5d47b432c08f50
Clayton Rogers <claytonrogers53@gmail.com>
2019-11-11 22:22:25 -0500

    FPGA: Increase ram size of 4k words

--- fpga/ram.v
+++ fpga/ram.v
@@ -15,22 +15,22 @@ parameter FILENAME = "empty.hex";
 parameter RAM_ADDR_WIDTH = 8; // unused for now
 
 // This will infer a ram block
-reg [15:0] mem [255:0];
+reg [15:0] mem [4096-1:0];
 initial $readmemh(FILENAME, mem);
 
 always @ ( posedge i_clk ) begin
-  if (write_strobe && 8'h00 == write_addr[15:8])
-    mem[write_addr[7:0]] <= write_data;
+  if (write_strobe && 4'h0 == write_addr[15:12])
+    mem[write_addr[11:0]] <= write_data;
 end
 
 reg addr_good;
 always @ ( posedge i_clk ) begin
-  addr_good <= 8'h00 == read_addr[15:8];
+  addr_good <= 4'h0 == read_addr[15:12];
 end
 
 reg [15:0] data_out;
 always @ ( posedge i_clk ) begin
-  data_out <= mem[read_addr[7:0]];
+  data_out <= mem[read_addr[11:0]];
 end
 
 assign read_data = (addr_good) ? data_out : 16'bzzzzzzzzzzzzzzzz;
