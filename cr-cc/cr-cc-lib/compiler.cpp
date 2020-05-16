a257f96db9e106cafaa094d171460c0f1836d1cf
Clayton Rogers <claytonrogers53@gmail.com>
2020-05-16 10:34:13 -0400

    Update compiler to accept .o as an input

--- cr-cc/cr-cc-lib/compiler.cpp
+++ cr-cc/cr-cc-lib/compiler.cpp
@@ -13,13 +13,17 @@ Compiler_Return compile_tu(std::string filename, FileReader f) {
 	if (file_extension == "c") {
 		// TODO if any include paths are specified add them here.
 		ret.assembly = c_to_asm(filename, f);
+		ret.item = assemble(ret.assembly);
 	} else if (file_extension == "s") {
 		ret.assembly = f.read_file_from_directories(filename);
+		ret.item = assemble(ret.assembly);
+	} else if (file_extension == "o") {
+		ret.assembly = "";
+		const auto stream = read_bin_file(filename);
+		ret.item = Object::Object_Container::from_stream(stream);
 	} else {
 		throw std::logic_error("Cannot handle file with extension: " + file_extension);
 	}
-
-	ret.item = assemble(ret.assembly);
 
 	return ret;
 }
