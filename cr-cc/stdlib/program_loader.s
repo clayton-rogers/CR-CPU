f6d4d6051828f94c5e82f188cb420f8e75c75341
Clayton Rogers <claytonrogers53@gmail.com>
2020-05-17 01:35:41 -0400

    Make program loader simpler

--- cr-cc/stdlib/program_loader.s
+++ cr-cc/stdlib/program_loader.s
@@ -7,12 +7,12 @@ loadi.h sp .RAM_SIZE
 sub sp, 1
 loadi ra .end
 loadi.h ra .end # load the return address for the program
-
-loadi rb .PROGRAM_ADDR
-loadi.h rb .PROGRAM_ADDR
 push ra
-push rb
-ret # jump to last pushed, i.e. rb, i.e PROGRAM_ADDR
+
+loadi ra .PROGRAM_ADDR
+loadi.h ra .PROGRAM_ADDR
+push ra
+ret # jump to last pushed, i.e PROGRAM_ADDR
 
 .end: # when the program returns halt
 halt
