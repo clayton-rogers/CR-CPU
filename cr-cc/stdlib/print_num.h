aacb2c6b6c867e3f82b81c0400ed481efd4eac61
Clayton Rogers <claytonrogers53@gmail.com>
2020-08-01 14:59:43 -0400

    Add print_num() to stdlib

---
+++ cr-cc/stdlib/print_num.h
@@ -0,0 +1,3 @@
+
+
+int print_num(int num);
---
+++ cr-cc/stdlib/print_num.s
@@ -0,0 +1,41 @@
+#################################################
+# print_hex(ra) prints a single hex
+# if the value is < 10 then add 48 to ascii char
+# if the value is >= 10 then sub 10 and add 65 to ascii uppercase
+.extern write_vga_char # from stdlib
+.print_hex:
+push ra
+sub ra 10  # >= 10
+jmp.r.gz ._is_letter
+load.sp ra 0
+add ra 48
+jmp.r ._print_after
+._is_letter:
+load.sp ra 0
+add ra 55
+._print_after:
+loada .write_vga_char
+call .write_vga_char
+add sp 1
+ret
+
+#################################################
+#int print_num(int num);
+.print_num:
+.export print_num
+push ra
+shftr ra 12
+call.r .print_hex
+load.sp ra 0
+shftr ra 8
+and ra 0x0F
+call.r .print_hex
+load.sp ra 0
+shftr ra 4
+and ra 0x0F
+call.r .print_hex
+load.sp ra 0
+and ra 0x0F
+call.r .print_hex
+add sp 1
+ret
