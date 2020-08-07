9b321f3be5874762c29b80f5a4a904660e0fd8c9
Clayton Rogers <claytonrogers53@gmail.com>
2020-08-07 01:19:51 -0400

    Fix sqrt in stdlib

--- cr-cc/stdlib/math_sqrt.c
+++ cr-cc/stdlib/math_sqrt.c
@@ -1,7 +1,12 @@
 
-
 int sqrt(int a) {
+	// TODO for some reason doesn't work with 1;
+	if (a == 1) { return 1; }
+	// TODO doesn't work for 3 because of the way the default guess works
+	if (a == 3) { return 1; }
+
 	int guess = a/2; // TODO change to shift
+	int last_last_guess = 0;
 	int last_guess = guess;
 
 	while (1) {
@@ -9,6 +14,12 @@ int sqrt(int a) {
 		if (guess == last_guess) {
 			break;
 		}
+		// using integers this algo is unstable for some numbers
+		if (guess == last_last_guess) {
+			guess = guess - 1;
+			break;
+		}
+		last_last_guess = last_guess;
 		last_guess = guess;
 	}
 
