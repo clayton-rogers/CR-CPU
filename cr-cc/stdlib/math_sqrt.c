bbf610831afa35d5d8e5eeba2330bd671486d07e
Clayton Rogers <claytonrogers53@gmail.com>
2020-08-08 00:04:30 -0400

    Make sqrt() simpler and quicker

--- cr-cc/stdlib/math_sqrt.c
+++ cr-cc/stdlib/math_sqrt.c
@@ -1,26 +1,15 @@
 
 int sqrt(int a) {
-	// TODO for some reason doesn't work with 1;
-	if (a == 1) { return 1; }
-	// TODO doesn't work for 3 because of the way the default guess works
-	if (a == 3) { return 1; }
-
-	int guess = a/2; // TODO change to shift
-	int last_last_guess = 0;
-	int last_guess = guess;
+	if (a == 0) { return 0; } // prevent div/0
+	int next_guess;
+	int guess = a/2 + 1;
 
 	while (1) {
-		guess = (last_guess + a/last_guess) / 2; // TODO again change to shift
-		if (guess == last_guess) {
+		next_guess = (guess + a/guess) / 2; // TODO change to shift
+		if (next_guess >= guess) {
 			break;
 		}
-		// using integers this algo is unstable for some numbers
-		if (guess == last_last_guess) {
-			guess = guess - 1;
-			break;
-		}
-		last_last_guess = last_guess;
-		last_guess = guess;
+		guess = next_guess;
 	}
 
 	return guess;
