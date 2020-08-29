90b7ebc020e3dcd1cbc45db8fda9eb9bd410a427
Clayton Rogers <claytonrogers53@gmail.com>
2020-08-29 17:38:55 -0400

    Change std square root to take advantage of shift

--- cr-cc/stdlib/math_sqrt.c
+++ cr-cc/stdlib/math_sqrt.c
@@ -2,10 +2,10 @@
 int sqrt(int a) {
 	if (a == 0) { return 0; } // prevent div/0
 	int next_guess;
-	int guess = a/2 + 1;
+	int guess = (a >> 1) + 1; // shift equivalent to div 2
 
 	while (1) {
-		next_guess = (guess + a/guess) / 2; // TODO change to shift
+		next_guess = (guess + a/guess) >> 1;
 		if (next_guess >= guess) {
 			break;
 		}
