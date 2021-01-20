a8ed1f4c4912e8d14b7164c8bfdbe7c88edafd94
Clayton Rogers <claytonrogers53@gmail.com>
2021-01-20 00:52:28 -0500

    Add more array tests

--- cr-cc/test_data/valid_c/array.c
+++ cr-cc/test_data/valid_c/array.c
@@ -10,8 +10,60 @@ int simple_array() {
 	return a[0] == 10 && a[1] == 12;
 }
 
+int array_complex_index(int index) {
+	int a[3];
+	a[0] = 11;
+	a[1] = 12;
+	a[2] = 13;
+
+	return a[index];
+}
+
+int array_complex_index_2() {
+	int a[3];
+	a[0] = 14;
+	a[1] = 15;
+	a[2] = 16;
+
+	int b = a[0 + 3 - 1] + a[7-1-5];
+
+	return b == (16 + 15);
+}
+
+int array_loop() {
+	int size = 10;
+	int a[10];
+
+	for (int i = 0; i < size; i = i + 1) {
+		a[i] = 32;
+	}
+
+	int total = 0;
+	for (int i = 0; i < size; i = i + 1) {
+		total = total + a[i];
+	}
+
+	return total == 320;
+}
+
 int main() {
+
 	if (!simple_array()) {
+		return 1;
+	}
+	if (array_complex_index(1) != 12) {
+		return 1;
+	}
+	if (array_complex_index(2) != 13) {
+		return 1;
+	}
+	if (array_complex_index(0) != 11) {
+		return 1;
+	}
+	if (!array_complex_index_2()) {
+		return 1;
+	}
+	if (!array_loop()) {
 		return 1;
 	}
 
