60633ec27e95159f4d6d037890c602d0d085728b
Clayton Rogers <claytonrogers53@gmail.com>
2021-02-10 14:59:41 -0500

    Update pins for hard board

--- fpga/top.v
+++ fpga/top.v
@@ -6,23 +6,24 @@ module top (
     output LED,   // User/boot LED next to power LED
     output USBPU,  // USB pull-up resistor
 
-    output PIN_1, // GPIO output pins
-    output PIN_3, // each has an LED
+    output PIN_3, // GPIO MSB
+    output PIN_4,
     output PIN_5,
+    output PIN_6,
     output PIN_7,
-    output PIN_10,
-    output PIN_11,
-    output PIN_12,
-    output PIN_13,
+    output PIN_8,
+    output PIN_9,
+    output PIN_10, // GPIO LSB
 
-    output PIN_14, // uart tx
-    input  PIN_15,  // uart rx
+    input  PIN_24, // uart rx
+    output PIN_23, // uart tx
 
-    output PIN_20, // blue
-    output PIN_21, // green
-    output PIN_22, // red
-    output PIN_23, // v_sync
-    output PIN_24  // h_sync
+    output PIN_22, // h_sync
+    output PIN_21, // v_sync
+    output PIN_20, // red
+    output PIN_19, // green
+    output PIN_18  // blue
+
     );
 
     localparam INPUT_CLOCK     = 16_000_000;
@@ -49,17 +50,17 @@ module top (
 
     // instantiate CPU
     wire [7:0] core_output;
-    assign {PIN_13, PIN_12, PIN_11, PIN_10, PIN_1, PIN_3, PIN_5, PIN_7} = core_output;
+    assign {PIN_3, PIN_4, PIN_5, PIN_6, PIN_7, PIN_8, PIN_9, PIN_10} = core_output;
     cpu cpu (
       .i_clk(CLK),
       .cpu_output(core_output),
-      .uart_rx_unsafe(PIN_15),
-      .uart_tx(PIN_14),
-      .vga_red(PIN_22),
-      .vga_green(PIN_21),
-      .vga_blue(PIN_20),
-      .vga_v_sync(PIN_23),
-      .vga_h_sync(PIN_24)
+      .uart_rx_unsafe(PIN_24),
+      .uart_tx(PIN_23),
+      .vga_red(PIN_20),
+      .vga_green(PIN_19),
+      .vga_blue(PIN_18),
+      .vga_v_sync(PIN_21),
+      .vga_h_sync(PIN_22)
     );
 
 endmodule
