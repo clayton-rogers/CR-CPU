3f956e6db03d7f79ed5424f52386ccea6757f4d6
Clayton Rogers <claytonrogers53@gmail.com>
2021-03-08 23:36:09 -0500

    Add rest of fake os project

---
+++ cr-cc/test_data/fake_os/main.s
@@ -0,0 +1,22 @@
+.extern user_load_srec
+.extern clear_screen
+.extern write_vga_char
+
+.main:
+.export main
+loada .clear_screen
+call .clear_screen
+loadi ra 0x65
+loada .write_vga_char
+call .write_vga_char
+loadi ra 0x65
+loada .write_vga_char
+call .write_vga_char
+loadi ra 0x65
+loada .write_vga_char
+call .write_vga_char
+
+loada .user_load_srec
+call .user_load_srec
+
+ret
