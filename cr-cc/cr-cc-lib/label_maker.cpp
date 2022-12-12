ee272323231c06a0e62ba55382c4b89e7d6bc0af
Clayton Rogers <claytonrogers53@gmail.com>
2022-12-12 01:46:34 -0500

    Fix autogenerated labels so that they won't clash with anything

--- cr-cc/cr-cc-lib/label_maker.cpp
+++ cr-cc/cr-cc-lib/label_maker.cpp
@@ -2,12 +2,12 @@
 
 std::string Label_Maker::get_next_label()
 {
-	return std::string(".L") + std::to_string(label_count++);
+	return std::string(".__L") + std::to_string(label_count++);
 }
 
 std::string Label_Maker::get_fn_end_label() const
 {
-	return std::string() + '.' + fn_name + "_end";
+	return std::string() + ".__" + fn_name + "_end";
 }
 
 void Label_Maker::set_fn_name(std::string fn)
