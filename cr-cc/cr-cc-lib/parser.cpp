e2c2bd47b4749cea7595e12406469c57dec65eea
Clayton Rogers <claytonrogers53@gmail.com>
2022-12-13 23:52:03 -0500

    Reformat parser to be easier to read

--- cr-cc/cr-cc-lib/parser.cpp
+++ cr-cc/cr-cc-lib/parser.cpp
@@ -13,13 +13,33 @@ struct Rule {
 
 using RuleList = std::vector<Rule>;
 
-const static std::map<TokenType, RuleList> C_GRAMMAR = {
+static std::map<TokenType, RuleList> C_TOP_LEVEL = {
 	{TokenType::translation_unit,
 		{
 			{{TokenType::external_declaration}, TokenType::external_declaration_tail},
 			//{{TokenType::function}, TokenType::function_tail},
 		}
 	},
+	{TokenType::compound_statement,
+		{
+			{{TokenType::open_bracket, TokenType::block_item_list, TokenType::close_bracket}, {}},
+			{{TokenType::open_bracket, TokenType::close_bracket}, {}},
+		}
+	},
+	{TokenType::block_item_list,
+		{
+			{{}, TokenType::block_item},
+		}
+	},
+	{TokenType::block_item,
+		{
+			{{TokenType::declaration}, {}},
+			{{TokenType::statement}, {}},
+		}
+	},
+};
+
+static std::map<TokenType, RuleList> C_DECLARATION = {
 	{TokenType::external_declaration_tail,
 		{
 			{{TokenType::external_declaration}, {}},
@@ -31,6 +51,57 @@ const static std::map<TokenType, RuleList> C_GRAMMAR = {
 			{{TokenType::declaration}, {}},
 		}
 	},
+	{TokenType::declaration_specifier,
+		{
+			{{TokenType::key_int}, {}},// TODO add other types and specifiers
+		}
+	},
+	{TokenType::declaration,
+		{
+			//{{TokenType::type_specifier, TokenType::semi_colon}, {}},// TODO for struct declaration
+			{{TokenType::declaration_specifier, TokenType::init_declarator_list, TokenType::semi_colon}, {}},
+		}
+	},
+	{TokenType::init_declarator_list,
+		{
+			{{TokenType::init_declarator}, TokenType::init_declarator_list_tail},
+		}
+	},
+	{TokenType::init_declarator_list_tail,
+		{
+			{{TokenType::comma, TokenType::init_declarator}, {}},
+		}
+	},
+	{TokenType::init_declarator,
+		{
+			{{TokenType::declarator, TokenType::equals, TokenType::expression}, {}},
+			{{TokenType::declarator}, {}},
+		}
+	},
+	{TokenType::declarator,
+		{
+			{{TokenType::pointer, TokenType::direct_declarator}, {}},
+			{{TokenType::direct_declarator}, {}},
+		}
+	},
+	{TokenType::direct_declarator,
+		{
+			{{TokenType::identifier}, TokenType::direct_declarator_tail}, // var/pointer
+		}
+	},
+	{TokenType::direct_declarator_tail,
+		{
+			{{TokenType::open_square_bracket, TokenType::constant, TokenType::close_square_bracket}, {}}, // technically should be const expression
+		}
+	},
+	{TokenType::pointer,
+		{
+			{{TokenType::star}, TokenType::star},
+		}
+	},
+};
+
+static std::map<TokenType, RuleList> C_FUNCTION = {
 	{TokenType::function,
 		{
 			{{TokenType::function_declaration}, {}},
@@ -71,99 +142,51 @@ const static std::map<TokenType, RuleList> C_GRAMMAR = {
 			{{TokenType::declaration_specifier}, {}},
 		}
 	},
-	{TokenType::declaration_specifier,
+	{TokenType::function_call,
 		{
-			{{TokenType::key_int}, {}},// TODO add other types and specifiers
+			{{TokenType::identifier, TokenType::open_parenth, TokenType::close_parenth}, {}},
+			{{TokenType::identifier, TokenType::open_parenth, TokenType::argument_expression_list, TokenType::close_parenth}, {}},
 		}
 	},
-	{TokenType::compound_statement,
+	{TokenType::argument_expression_list,
 		{
-			{{TokenType::open_bracket, TokenType::block_item_list, TokenType::close_bracket}, {}},
-			{{TokenType::open_bracket, TokenType::close_bracket}, {}},
+			{{TokenType::expression}, TokenType::argument_expression_list_tail},
 		}
 	},
-	{TokenType::block_item_list,
+	{TokenType::argument_expression_list_tail,
 		{
-			{{}, TokenType::block_item},
+			{{TokenType::comma, TokenType::expression}, {}},
 		}
 	},
-	{TokenType::block_item,
+};
+
+static std::map<TokenType, RuleList> C_STATEMENT = {
+	{TokenType::statement,
 		{
-			{{TokenType::declaration}, {}},
-			{{TokenType::statement}, {}},
+			{{TokenType::compound_statement}, {}},
+			{{TokenType::jump_statement}, {}},
+			{{TokenType::if_statement}, {}},
+			{{TokenType::for_statement}, {}},
+			{{TokenType::while_statement}, {}},
+			{{TokenType::do_while_statement}, {}},
+			{{TokenType::break_statement}, {}},
+			{{TokenType::continue_statement}, {}},
+			{{TokenType::expression_statement}, {}},
+			// TODO add other types of statements
 		}
 	},
-	{TokenType::declaration,
+	{TokenType::jump_statement,
 		{
-		//{{TokenType::type_specifier, TokenType::semi_colon}, {}},// TODO for struct declaration
-		{{TokenType::declaration_specifier, TokenType::init_declarator_list, TokenType::semi_colon}, {}},
-	}
-},
-{TokenType::init_declarator_list,
-	{
-		{{TokenType::init_declarator}, TokenType::init_declarator_list_tail},
-	}
-},
-{TokenType::init_declarator_list_tail,
-	{
-		{{TokenType::comma, TokenType::init_declarator}, {}},
-	}
-},
-{TokenType::init_declarator,
-	{
-		{{TokenType::declarator, TokenType::equals, TokenType::expression}, {}},
-		{{TokenType::declarator}, {}},
-	}
-},
-{TokenType::declarator,
-	{
-		{{TokenType::pointer, TokenType::direct_declarator}, {}},
-		{{TokenType::direct_declarator}, {}},
-	}
-},
-{TokenType::direct_declarator,
-	{
-		{{TokenType::identifier}, TokenType::direct_declarator_tail}, // var/pointer
-	}
-},
-{TokenType::direct_declarator_tail,
-	{
-		{{TokenType::open_square_bracket, TokenType::constant, TokenType::close_square_bracket}, {}}, // technically should be const expression
-	}
-},
-{TokenType::pointer,
-	{
-		{{TokenType::star}, TokenType::star},
-	}
-},
-{TokenType::statement,
-	{
-		{{TokenType::compound_statement}, {}},
-		{{TokenType::jump_statement}, {}},
-		{{TokenType::if_statement}, {}},
-		{{TokenType::for_statement}, {}},
-		{{TokenType::while_statement}, {}},
-		{{TokenType::do_while_statement}, {}},
-		{{TokenType::break_statement}, {}},
-		{{TokenType::continue_statement}, {}},
-		{{TokenType::expression_statement}, {}},
-		// TODO add other types of statements
-	}
-},
-{TokenType::jump_statement,
-	{
-		// TODO for now don't accept return without an expression
-		//{{TokenType::key_return, TokenType::semi_colon}, {}},
-		{{TokenType::key_return, TokenType::expression, TokenType::semi_colon}, {}},
-		// TODO add other types of jumps (break, continue, goto)
-	}
-},
-{TokenType::if_statement,
-	{
-		// If with else
-		{{TokenType::key_if, TokenType::open_parenth, TokenType::expression, TokenType::close_parenth,
-			TokenType::statement, TokenType::key_else, TokenType::statement}, {}},
-			// If without else
+			// TODO for now don't accept return without an expression
+			//{{TokenType::key_return, TokenType::semi_colon}, {}},
+			{{TokenType::key_return, TokenType::expression, TokenType::semi_colon}, {}},
+			// TODO add other types of jumps (break, continue, goto)
+		}
+	},
+	{TokenType::if_statement,
+		{
+			{{TokenType::key_if, TokenType::open_parenth, TokenType::expression, TokenType::close_parenth,
+				TokenType::statement, TokenType::key_else, TokenType::statement}, {}},
 			{{TokenType::key_if, TokenType::open_parenth, TokenType::expression, TokenType::close_parenth,
 				TokenType::statement}, {}},
 		}
@@ -185,6 +208,25 @@ const static std::map<TokenType, RuleList> C_GRAMMAR = {
 			TokenType::for_condition_expression, TokenType::for_increment_expression, TokenType::statement}, {}},
 		}
 	},
+	{TokenType::break_statement,
+		{
+			{{TokenType::key_break, TokenType::semi_colon}, {}},
+		}
+	},
+	{TokenType::continue_statement,
+		{
+			{{TokenType::key_continue, TokenType::semi_colon}, {}},
+		}
+	},
+	{TokenType::expression_statement,
+		{
+			{{TokenType::expression, TokenType::semi_colon}, {}},
+			{{TokenType::semi_colon}, {}},
+		}
+	},
+};
+
+static std::map<TokenType, RuleList> C_EXPRESSION = {
 	{TokenType::for_init_expression,
 		{
 			{{TokenType::declaration}, {}},
@@ -194,174 +236,159 @@ const static std::map<TokenType, RuleList> C_GRAMMAR = {
 	},
 	{TokenType::for_condition_expression,
 		{
-		// Condition expression is optional, if omitted it is assumed 1
-		{{TokenType::expression, TokenType::semi_colon}, {}},
-		{{TokenType::semi_colon}, {}},
-	}
-},
-{TokenType::for_increment_expression,
-	{
-		// Incremente expression is optional. We must include the close parenth
-		// because every non-terminal token must consume at least one token to be valid
-		{{TokenType::expression, TokenType::close_parenth}, {}},
-		{{TokenType::close_parenth}, {}},
-	}
-},
-{TokenType::break_statement,
-	{
-		{{TokenType::key_break, TokenType::semi_colon}, {}},
-	}
-},
-{TokenType::continue_statement,
-	{
-		{{TokenType::key_continue, TokenType::semi_colon}, {}},
-	}
-},
-{TokenType::expression_statement,
-	{
-		{{TokenType::expression, TokenType::semi_colon}, {}},
-		{{TokenType::semi_colon}, {}},
-	}
-},
-{TokenType::expression,
-	{
-		{{TokenType::unary_expression, TokenType::equals, TokenType::expression}, {}},
-		{{TokenType::conditional_exp}, {}},
-	}
-},
-{TokenType::conditional_exp,
-	{
-		{{TokenType::logical_or_exp}, TokenType::logical_or_exp_tail},
-	}
-},
-{TokenType::logical_or_exp_tail,
-	{
-		{{TokenType::question, TokenType::expression, TokenType::colon, TokenType::conditional_exp}, {}},
-	}
-},
-{TokenType::logical_or_exp,
-	{
-		{{TokenType::logical_and_exp}, TokenType::logical_and_exp_tail},
-	}
-},
-{TokenType::logical_and_exp_tail,
-	{
-		{{TokenType::or_op, TokenType::logical_and_exp}, {}},
-	}
-},
-{TokenType::logical_and_exp,
-	{
-		{{TokenType::equality_exp}, TokenType::equality_exp_tail},
-	}
-},
-{TokenType::equality_exp_tail,
-	{
-		{{TokenType::and_op, TokenType::equality_exp}, {}},
-	}
-},
-{TokenType::equality_exp,
-	{
-		{{TokenType::relational_exp}, TokenType::relational_exp_tail},
-	}
-},
-{TokenType::relational_exp_tail,
-	{
-		{{TokenType::ne_op, TokenType::relational_exp}, {}},
-		{{TokenType::eq_op, TokenType::relational_exp}, {}},
-	}
-},
-{TokenType::relational_exp,
-	{
-		{{TokenType::shift_exp}, TokenType::shift_exp_tail},
-	}
-},
-{TokenType::shift_exp_tail,
-	{
-		{{TokenType::less_than, TokenType::shift_exp}, {}},
-		{{TokenType::greater_than, TokenType::shift_exp}, {}},
-		{{TokenType::le_op, TokenType::shift_exp}, {}},
-		{{TokenType::ge_op, TokenType::shift_exp}, {}},
-	}
-},
-{TokenType::shift_exp,
-	{
-		{{TokenType::additive_exp}, TokenType::additive_exp_tail},
-	}
-},
-{ TokenType::additive_exp_tail,
-	{
-		{{TokenType::left_op, TokenType::additive_exp}, {}},
-		{{TokenType::right_op, TokenType::additive_exp}, {}},
-	}
-},
-{TokenType::additive_exp,
-	{
-		{{TokenType::term}, TokenType::term_tail},
-	}
-},
-{TokenType::term_tail,
-	{
-		{{TokenType::add, TokenType::term}, {}},
-		{{TokenType::sub, TokenType::term}, {}},
-	}
-},
-{TokenType::term,
-	{
-		{{TokenType::factor}, TokenType::factor_tail},
-	}
-},
-{TokenType::factor_tail,
-	{
-		{{TokenType::star, TokenType::factor}, {}}, // mult
-		{{TokenType::div, TokenType::factor}, {}}, // div
-		{{TokenType::percent, TokenType::factor}, {}}, // modulo
-	}
-},
-{TokenType::factor,
-	{
-		{{TokenType::open_parenth, TokenType::expression, TokenType::close_parenth}, {}},
-		{{TokenType::function_call}, {}},
-		{{TokenType::unary_expression}, {}},
-		{{TokenType::constant}, {}},
-	}
-},
-{TokenType::function_call,
-	{
-		{{TokenType::identifier, TokenType::open_parenth, TokenType::close_parenth}, {}},
-		{{TokenType::identifier, TokenType::open_parenth, TokenType::argument_expression_list, TokenType::close_parenth}, {}},
-	}
-},
-{TokenType::argument_expression_list,
-	{
-		{{TokenType::expression}, TokenType::argument_expression_list_tail},
-	}
-},
-{TokenType::argument_expression_list_tail,
-	{
-		{{TokenType::comma, TokenType::expression}, {}},
-	}
-},
-{TokenType::unary_expression,
-	{
-		{{TokenType::sub, TokenType::factor}, {}},
-		{{TokenType::tilda, TokenType::factor}, {}},
-		{{TokenType::exclam, TokenType::factor}, {}},
-		{{TokenType::ampersand, TokenType::identifier}, {}},
-		{{TokenType::star, TokenType::identifier}, {}},
-		{{TokenType::inc_op, TokenType::identifier}, {}},
-		{{TokenType::dec_op, TokenType::identifier}, {}},
-		{{TokenType::identifier, TokenType::open_square_bracket, TokenType::expression, TokenType::close_square_bracket}, {}},
-		{{TokenType::identifier}, {}},
-	}
-},
+			// Condition expression is optional, if omitted it is assumed 1
+			{{TokenType::expression, TokenType::semi_colon}, {}},
+			{{TokenType::semi_colon}, {}},
+		}
+	},
+	{TokenType::for_increment_expression,
+		{
+			// Incremente expression is optional. We must include the close parenth
+			// because every non-terminal token must consume at least one token to be valid
+			{{TokenType::expression, TokenType::close_parenth}, {}},
+			{{TokenType::close_parenth}, {}},
+		}
+	},
+	{TokenType::expression,
+		{
+			{{TokenType::unary_expression, TokenType::equals, TokenType::expression}, {}},
+			{{TokenType::conditional_exp}, {}},
+		}
+	},
+	{TokenType::conditional_exp,
+		{
+			{{TokenType::logical_or_exp}, TokenType::logical_or_exp_tail},
+		}
+	},
+	{TokenType::logical_or_exp_tail,
+		{
+			{{TokenType::question, TokenType::expression, TokenType::colon, TokenType::conditional_exp}, {}},
+		}
+	},
+	{TokenType::logical_or_exp,
+		{
+			{{TokenType::logical_and_exp}, TokenType::logical_and_exp_tail},
+		}
+	},
+	{TokenType::logical_and_exp_tail,
+		{
+			{{TokenType::or_op, TokenType::logical_and_exp}, {}},
+		}
+	},
+	{TokenType::logical_and_exp,
+		{
+			{{TokenType::equality_exp}, TokenType::equality_exp_tail},
+		}
+	},
+	{TokenType::equality_exp_tail,
+		{
+			{{TokenType::and_op, TokenType::equality_exp}, {}},
+		}
+	},
+	{TokenType::equality_exp,
+		{
+			{{TokenType::relational_exp}, TokenType::relational_exp_tail},
+		}
+	},
+	{TokenType::relational_exp_tail,
+		{
+			{{TokenType::ne_op, TokenType::relational_exp}, {}},
+			{{TokenType::eq_op, TokenType::relational_exp}, {}},
+		}
+	},
+	{TokenType::relational_exp,
+		{
+			{{TokenType::shift_exp}, TokenType::shift_exp_tail},
+		}
+	},
+	{TokenType::shift_exp_tail,
+		{
+			{{TokenType::less_than, TokenType::shift_exp}, {}},
+			{{TokenType::greater_than, TokenType::shift_exp}, {}},
+			{{TokenType::le_op, TokenType::shift_exp}, {}},
+			{{TokenType::ge_op, TokenType::shift_exp}, {}},
+		}
+	},
+	{TokenType::shift_exp,
+		{
+			{{TokenType::additive_exp}, TokenType::additive_exp_tail},
+		}
+	},
+	{TokenType::additive_exp_tail,
+		{
+			{{TokenType::left_op, TokenType::additive_exp}, {}},
+			{{TokenType::right_op, TokenType::additive_exp}, {}},
+		}
+	},
+	{TokenType::additive_exp,
+		{
+			{{TokenType::term}, TokenType::term_tail},
+		}
+	},
+	{TokenType::term_tail,
+		{
+			{{TokenType::add, TokenType::term}, {}},
+			{{TokenType::sub, TokenType::term}, {}},
+		}
+	},
+	{TokenType::term,
+		{
+			{{TokenType::factor}, TokenType::factor_tail},
+		}
+	},
+	{TokenType::factor_tail,
+		{
+			{{TokenType::star, TokenType::factor}, {}}, // mult
+			{{TokenType::div, TokenType::factor}, {}}, // div
+			{{TokenType::percent, TokenType::factor}, {}}, // modulo
+		}
+	},
+	{TokenType::factor,
+		{
+			{{TokenType::open_parenth, TokenType::expression, TokenType::close_parenth}, {}},
+			{{TokenType::function_call}, {}},
+			{{TokenType::unary_expression}, {}},
+			{{TokenType::constant}, {}},
+		}
+	},
+	{TokenType::unary_expression,
+		{
+			{{TokenType::sub, TokenType::factor}, {}},
+			{{TokenType::tilda, TokenType::factor}, {}},
+			{{TokenType::exclam, TokenType::factor}, {}},
+			{{TokenType::ampersand, TokenType::identifier}, {}},
+			{{TokenType::star, TokenType::identifier}, {}},
+			{{TokenType::inc_op, TokenType::identifier}, {}},
+			{{TokenType::dec_op, TokenType::identifier}, {}},
+			{{TokenType::identifier, TokenType::open_square_bracket, TokenType::expression, TokenType::close_square_bracket}, {}},
+			{{TokenType::identifier}, {}},
+		}
+	},
 };
+
+static const std::map<TokenType, RuleList>* get_c_grammar()
+{
+	static std::map<TokenType, RuleList> C_GRAMMAR;
+	static bool needs_init = true;
+
+	if (needs_init) {
+		C_GRAMMAR.merge(C_DECLARATION);
+		C_GRAMMAR.merge(C_EXPRESSION);
+		C_GRAMMAR.merge(C_FUNCTION);
+		C_GRAMMAR.merge(C_STATEMENT);
+		C_GRAMMAR.merge(C_TOP_LEVEL);
+		needs_init = false;
+	}
+
+	return &C_GRAMMAR;
+}
 
 static bool is_token_terminal(TokenType token)
 {
 	// A token is terminal when it can no longer be broken into more tokens,
 	// in this case the token will not appear in the grammar and will have been
 	// recognized by the tokenizer.
-	return C_GRAMMAR.count(token) != 1;
+	return get_c_grammar()->count(token) != 1;
 }
 
 static int parse_node(ParseNode* node, const TokenList& token_list, int offset)
@@ -374,7 +401,7 @@ static int parse_node(ParseNode* node, const TokenList& token_list, int offset)
 			return 0; // No match
 		}
 	} else {
-		RuleList rule_list = C_GRAMMAR.at(node->token.token_type);
+		RuleList rule_list = get_c_grammar()->at(node->token.token_type);
 
 		for (const Rule& rule : rule_list) {
 			int consumed_tokens = 0;
